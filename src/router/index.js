ad91196df299cd3745fd5b8e067fdcc19bdce0bc
Paul Kononenko <storuky@gmail.com>
2019-01-07 13:38:53 +0300

    lint router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import store from '../store/store'
+import Vue from "vue";
+import Router from "vue-router";
+import store from "../store/store";
 // import $$ from '../assets/js/methods'
 // import index from '@/components/web/page/index'
 
@@ -30,149 +30,209 @@ import store from '../store/store'
 // import backupWallet from '@/components/web/page/backupWallet/index'
 // import backupSure from '@/components/web/page/backupWallet/backupSure'
 
-Vue.use(Router)
+Vue.use(Router);
 
 const routers = new Router({
   // mode: 'history',
   routes: [
     {
-      path: '/',
+      path: "/",
       // name: 'index',
-      component: resolve => require(['@/components/web/page/index'], resolve), // index,
+      component: resolve => require(["@/components/web/page/index"], resolve), // index,
       children: [
         {
-          path: '/',
-          component: resolve => require(['@/components/web/page/createWallet/index'], resolve) // biticonView
-        }, {
-          path: '/biticonView',
-          name: 'biticonView',
-          component: resolve => require(['@/components/web/page/createWallet/index'], resolve) // biticonView
-        }, {
-          path: '/createWallet',
-          name: 'createWallet',
-          component: resolve => require(['@/components/web/page/createWallet/create'], resolve) // createWallet
-        }, {
-          path: '/importWallet',
-          name: 'importWallet',
-          component: resolve => require(['@/components/web/page/createWallet/import'], resolve) // importWallet
-        }, {
-          path: '/saveKeystore',
-          name: 'saveKeystore',
-          component: resolve => require(['@/components/web/page/createWallet/savekeystore'], resolve) // saveKeystore
-        }, {
-          path: '/backupSure',
-          name: 'backupSure',
-          component: resolve => require(['@/components/web/page/backupWallet/backupSure'], resolve) // backupSure
-        }, {
-          path: '/nav',
+          path: "/",
+          component: resolve =>
+            require(["@/components/web/page/createWallet/index"], resolve) // biticonView
+        },
+        {
+          path: "/biticonView",
+          name: "biticonView",
+          component: resolve =>
+            require(["@/components/web/page/createWallet/index"], resolve) // biticonView
+        },
+        {
+          path: "/createWallet",
+          name: "createWallet",
+          component: resolve =>
+            require(["@/components/web/page/createWallet/create"], resolve) // createWallet
+        },
+        {
+          path: "/importWallet",
+          name: "importWallet",
+          component: resolve =>
+            require(["@/components/web/page/createWallet/import"], resolve) // importWallet
+        },
+        {
+          path: "/saveKeystore",
+          name: "saveKeystore",
+          component: resolve =>
+            require([
+              "@/components/web/page/createWallet/savekeystore"
+            ], resolve) // saveKeystore
+        },
+        {
+          path: "/backupSure",
+          name: "backupSure",
+          component: resolve =>
+            require(["@/components/web/page/backupWallet/backupSure"], resolve) // backupSure
+        },
+        {
+          path: "/nav",
           // name: 'nav',
-          component: resolve => require(['@/components/web/page/nav'], resolve), // navLeft,
+          component: resolve => require(["@/components/web/page/nav"], resolve), // navLeft,
           children: [
             {
-              path: '/',
-              component: resolve => require(['@/components/web/page/myAssets/index'], resolve) // myAssets
-            }, {
-              path: '/MyAssets',
-              name: 'MyAssets',
-              component: resolve => require(['@/components/web/page/myAssets/index'], resolve), // myAssets,
+              path: "/",
+              component: resolve =>
+                require(["@/components/web/page/myAssets/index"], resolve) // myAssets
+            },
+            {
+              path: "/MyAssets",
+              name: "MyAssets",
+              component: resolve =>
+                require(["@/components/web/page/myAssets/index"], resolve), // myAssets,
               children: [
                 {
-                  path: '/pwdMyAssets',
-                  component: resolve => require(['@/components/web/page/public/pwdSure'], resolve) // pwdSure
+                  path: "/pwdMyAssets",
+                  component: resolve =>
+                    require(["@/components/web/page/public/pwdSure"], resolve) // pwdSure
                 }
               ]
-            }, {
-              path: '/Transfer',
+            },
+            {
+              path: "/Transfer",
               // name: 'Transfer',
-              component: resolve => require(['@/components/web/page/transfer/index'], resolve), // transfer,
+              component: resolve =>
+                require(["@/components/web/page/transfer/index"], resolve), // transfer,
               children: [
                 {
-                  path: '/',
-                  component: resolve => require(['@/components/web/page/transfer/receive'], resolve) // tranReceive
-                }, {
-                  path: '/Transfer/tranReceive',
-                  name: 'tranReceive',
-                  component: resolve => require(['@/components/web/page/transfer/receive'], resolve) // tranReceive
-                }, {
-                  path: '/Transfer/tranSend',
-                  name: 'tranSend',
-                  component: resolve => require(['@/components/web/page/transfer/send'], resolve), // tranSend,
+                  path: "/",
+                  component: resolve =>
+                    require(["@/components/web/page/transfer/receive"], resolve) // tranReceive
+                },
+                {
+                  path: "/Transfer/tranReceive",
+                  name: "tranReceive",
+                  component: resolve =>
+                    require(["@/components/web/page/transfer/receive"], resolve) // tranReceive
+                },
+                {
+                  path: "/Transfer/tranSend",
+                  name: "tranSend",
+                  component: resolve =>
+                    require(["@/components/web/page/transfer/send"], resolve), // tranSend,
                   children: [
                     {
-                      path: '/pwdSend',
-                      component: resolve => require(['@/components/web/page/public/pwdSure'], resolve) // pwdSure
+                      path: "/pwdSend",
+                      component: resolve =>
+                        require([
+                          "@/components/web/page/public/pwdSure"
+                        ], resolve) // pwdSure
                     }
                   ]
                 }
               ]
-            }, {
-              path: '/LILO',
+            },
+            {
+              path: "/LILO",
               // name: 'LILO',
-              component: resolve => require(['@/components/web/page/lilo/index'], resolve), // lilo,
+              component: resolve =>
+                require(["@/components/web/page/lilo/index"], resolve), // lilo,
               children: [
                 {
-                  path: '/',
-                  component: resolve => require(['@/components/web/page/lilo/coinList'], resolve), // coinList,
+                  path: "/",
+                  component: resolve =>
+                    require(["@/components/web/page/lilo/coinList"], resolve), // coinList,
                   children: [
                     {
-                      path: '/pwdCoinList',
-                      component: resolve => require(['@/components/web/page/public/pwdSure'], resolve) // pwdSure
+                      path: "/pwdCoinList",
+                      component: resolve =>
+                        require([
+                          "@/components/web/page/public/pwdSure"
+                        ], resolve) // pwdSure
                     }
                   ]
-                }, {
-                  path: '/lockNav',
+                },
+                {
+                  path: "/lockNav",
                   // name: 'lockNav',
-                  component: resolve => require(['@/components/web/page/lilo/lockNav'], resolve), // lockNav,
+                  component: resolve =>
+                    require(["@/components/web/page/lilo/lockNav"], resolve), // lockNav,
                   children: [
                     {
-                      path: '/',
-                      component: resolve => require(['@/components/web/page/lilo/lockIn'], resolve) // lockIn
-                    }, {
-                      path: '/LILO/lockIn',
-                      name: 'lockIn',
-                      component: resolve => require(['@/components/web/page/lilo/lockIn'], resolve), // lockIn,
+                      path: "/",
+                      component: resolve =>
+                        require(["@/components/web/page/lilo/lockIn"], resolve) // lockIn
+                    },
+                    {
+                      path: "/LILO/lockIn",
+                      name: "lockIn",
+                      component: resolve =>
+                        require(["@/components/web/page/lilo/lockIn"], resolve), // lockIn,
                       children: [
                         {
-                          path: '/pwdLockIn',
-                          component: resolve => require(['@/components/web/page/public/pwdSure'], resolve) // pwdSure
+                          path: "/pwdLockIn",
+                          component: resolve =>
+                            require([
+                              "@/components/web/page/public/pwdSure"
+                            ], resolve) // pwdSure
                         }
                       ]
-                    }, {
-                      path: '/LILO/lockOut',
-                      name: 'lockOut',
-                      component: resolve => require(['@/components/web/page/lilo/lockOut'], resolve), // lockOut,
+                    },
+                    {
+                      path: "/LILO/lockOut",
+                      name: "lockOut",
+                      component: resolve =>
+                        require([
+                          "@/components/web/page/lilo/lockOut"
+                        ], resolve), // lockOut,
                       children: [
                         {
-                          path: '/pwdLockOut',
-                          component: resolve => require(['@/components/web/page/public/pwdSure'], resolve) // pwdSure
+                          path: "/pwdLockOut",
+                          component: resolve =>
+                            require([
+                              "@/components/web/page/public/pwdSure"
+                            ], resolve) // pwdSure
                         }
                       ]
                     }
                   ]
                 }
               ]
-            }, {
-              path: '/DAPP',
-              name: 'DAPP',
-              component: resolve => require(['@/components/web/page/dapp/index'], resolve) // dapp
-            }, {
-              path: '/backupWallet',
-              name: 'backupWallet',
-              component: resolve => require(['@/components/web/page/backupWallet/index'], resolve) // backupWallet
-            }, {
-              path: '/loading',
-              name: 'loading',
-              component: resolve => require(['@/components/web/page/public/loading'], resolve) // loading
+            },
+            {
+              path: "/DAPP",
+              name: "DAPP",
+              component: resolve =>
+                require(["@/components/web/page/dapp/index"], resolve) // dapp
+            },
+            {
+              path: "/backupWallet",
+              name: "backupWallet",
+              component: resolve =>
+                require(["@/components/web/page/backupWallet/index"], resolve) // backupWallet
+            },
+            {
+              path: "/loading",
+              name: "loading",
+              component: resolve =>
+                require(["@/components/web/page/public/loading"], resolve) // loading
             }
           ]
         }
       ]
     }
   ]
-})
+});
 
 routers.beforeEach((to, from, next) => {
-  const showSearchURL = ['biticonView', 'createWallet', 'importWallet', 'saveKeystore']
+  const showSearchURL = [
+    "biticonView",
+    "createWallet",
+    "importWallet",
+    "saveKeystore"
+  ];
   // console.log(this.$store)
   // if (!this.$store.state.storePrivateKey) {
   //   this.$router.push('/')
@@ -184,32 +244,38 @@ routers.beforeEach((to, from, next) => {
   // }
   // console.log($('.OnLodaingIndex').length)
   if (store.state.addressInfo) {
-    next()
+    next();
     // console.log(1)
   } else {
-    if (to.path === '/' || to.path === '/importWallet' || to.path === '/createWallet' || to.path === '/saveKeystore' || to.path === '/backupSure') {
-      next()
+    if (
+      to.path === "/" ||
+      to.path === "/importWallet" ||
+      to.path === "/createWallet" ||
+      to.path === "/saveKeystore" ||
+      to.path === "/backupSure"
+    ) {
+      next();
       // console.log(2)
     } else {
-      next('/')
+      next("/");
       // console.log(3)
     }
   }
   // console.log(store.state.addressInfo)
   // next()
-  let toPath = to.path.toUpperCase()
-  if (toPath === '/') {
-    $('#topSearchView').show()
-    $('#topSetView').hide()
+  let toPath = to.path.toUpperCase();
+  if (toPath === "/") {
+    $("#topSearchView").show();
+    $("#topSetView").hide();
   } else {
     for (let i = 0; i < showSearchURL.length; i++) {
       if (toPath.indexOf(showSearchURL[i].toUpperCase()) !== -1) {
-        $('#topSearchView').show()
-        $('#topSetView').hide()
-        break
+        $("#topSearchView").show();
+        $("#topSetView").hide();
+        break;
       } else {
-        $('#topSearchView').hide()
-        $('#topSetView').show()
+        $("#topSearchView").hide();
+        $("#topSetView").show();
       }
     }
   }
@@ -225,6 +291,6 @@ routers.beforeEach((to, from, next) => {
   //   }, delayTime)
   // }
   // next()
-})
+});
 
-export default routers
+export default routers;
